ff7522670c6efaaa36149e66b5769d472fbafe95
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-18 17:27:54 +0300

    Squashed commit of the following:
    commit c7da1c4c015201f548bd4ad24cf66c0a61f960e6
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 17:27:37 2026 +0300
    
        Create 0001_initial.py
    
    commit 2659e027b6528ee31a42c9e392f50c2702a47a9a
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 16:52:02 2026 +0300
    
        Update models.py
    
    commit 38f113963c92fcecce4ddda640a6d598f48f55ab
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 16:50:08 2026 +0300
    
        Update admin.py
    
    commit 3be98801f9d69ddfbb18c4eebcf01b141ceab767
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 16:44:25 2026 +0300
    
        Update models.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Favorite, Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "owner",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "description",
+        "owner__email",
+        "owner__name",
+        "owner__surname",
+    )
+    ordering = (
+        "-created_at",
+    )
+    filter_horizontal = (
+        "participants",
+    )
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "project",
+        "created_at",
+    )
+    list_filter = (
+        "created_at",
+    )
+    search_fields = (
+        "user__email",
+        "user__name",
+        "user__surname",
+        "project__name",
+    )
+    ordering = (
+        "-created_at",
+    )
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,84 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "active", "Активен"
+        COMPLETED = "completed", "Завершён"
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_projects",
+        verbose_name="автор",
+    )
+    name = models.CharField(
+        "название",
+        max_length=200,
+    )
+    description = models.TextField(
+        "описание",
+        blank=True,
+    )
+    status = models.CharField(
+        "статус",
+        max_length=20,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="participating_projects",
+        blank=True,
+        verbose_name="участники",
+    )
+    created_at = models.DateTimeField(
+        "дата публикации",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        "дата изменения",
+        auto_now=True,
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("projects:detail", kwargs={"pk": self.pk})
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="favorites",
+        verbose_name="пользователь",
+    )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="favorites",
+        verbose_name="проект",
+    )
+    created_at = models.DateTimeField(
+        "дата добавления",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "project"],
+                name="unique_user_favorite_project",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} - {self.project}"
