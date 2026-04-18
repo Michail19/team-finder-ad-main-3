ee24ad9ebe0915a33b7e10a47fa9e761232278b3
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-18 14:41:08 +0300

    Squashed commit of the following:
    commit 2d0ffee640287266a84b201ea1f113f129ccd6da
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 14:35:12 2026 +0300
    
        Added custom UserManager
    
    commit fbe2d02e01758bf2deee4d86b261ecd3261f2711
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 14:15:38 2026 +0300
    
        Create 0001_initial.py
    
    commit 170c5e6f23460d2f9a51b5730ac5b60cb22360ae
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 14:10:23 2026 +0300
    
        Update admin.py
    
    commit bb378a24f892a4ada5326f56a9f2953210f12f73
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 13:09:59 2026 +0300
    
        Update settings.py
    
    commit 94a988a77d938719a9ec3cd1d9342741674cc28c
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Sat Apr 18 13:06:56 2026 +0300
    
        Update models.py
    
    commit 0acd68010485615efdd6d8ba211363f0a2569eea
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Fri Apr 17 14:56:19 2026 +0300
    
        Update models.py

--- team_finder/settings.py
+++ team_finder/settings.py
@@ -83,24 +83,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = []
-if not DEBUG:
-    AUTH_PASSWORD_VALIDATORS.extend(
-        [
-            {
-                "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-            },
-            {
-                "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-            },
-        ]
-    )
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -127,3 +123,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,82 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Поле email должно быть заполнено.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+
+    name = models.CharField(
+        "имя",
+        max_length=150,
+    )
+    surname = models.CharField(
+        "фамилия",
+        max_length=150,
+    )
+    avatar = models.ImageField(
+        "аватар",
+        upload_to="avatars/",
+        blank=True,
+        null=True,
+    )
+    about = models.TextField(
+        "описание",
+        blank=True,
+    )
+    email = models.EmailField(
+        "email",
+        unique=True,
+    )
+    phone = models.CharField(
+        "телефон",
+        max_length=32,
+        blank=True,
+    )
+    github_url = models.URLField(
+        "GitHub",
+        blank=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "surname"]
+
+    objects = UserManager()
+
+    class Meta:
+        ordering = ["-date_joined"]
+
+    def __str__(self):
+        return f"{self.name} {self.surname}".strip() or self.email
