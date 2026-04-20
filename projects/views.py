48cf1f1c1a259f0b6eda3fbfc22a252f96a28f67
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-20 14:47:31 +0300

    Squashed commit of the following:
    commit 654f107a5f2cd68acb2b6d3b5d9f4ebf1d5f2d16
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Mon Apr 20 14:46:52 2026 +0300
    
        Update urls.py
    
    commit 82ff4738f5c5bf74bf1bf932d65a7535b76827f4
    Author: Michail19 <Mi.ershov.2017@yandex.ru>
    Date:   Mon Apr 20 14:45:40 2026 +0300
    
        Added views.py realization

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .forms import ProjectForm
-from .models import Project
+from .models import Favorite, Project
 
 
 class ProjectListView(ListView):
@@ -19,6 +19,22 @@ class ProjectListView(ListView):
             Project.objects.select_related("owner")
             .prefetch_related("participants")
             .order_by("-created_at")
+        )
+
+
+class FavoriteProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "projects/favorite_projects.html"
+    context_object_name = "projects"
+    paginate_by = 12
+
+    def get_queryset(self):
+        return (
+            Project.objects.filter(favorites__user=self.request.user)
+            .select_related("owner")
+            .prefetch_related("participants")
+            .order_by("-favorites__created_at")
+            .distinct()
         )
 
 
@@ -89,3 +105,33 @@ def complete_project(request, pk):
         "project_status_display": "Закрыт",
         "project_id": project.pk,
     })
+
+
+@login_required
+def toggle_favorite(request, pk):
+    if request.method != "POST":
+        return JsonResponse(
+            {"status": "error", "message": "Метод не поддерживается"},
+            status=405,
+        )
+
+    project = get_object_or_404(Project, pk=pk)
+
+    favorite, created = Favorite.objects.get_or_create(
+        user=request.user,
+        project=project,
+    )
+
+    if created:
+        is_favorite = True
+    else:
+        favorite.delete()
+        is_favorite = False
+
+    return JsonResponse(
+        {
+            "status": "ok",
+            "is_favorite": is_favorite,
+            "project_id": project.pk,
+        }
+    )
